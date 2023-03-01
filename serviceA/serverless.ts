7d533b2d742ca5c3dcc93c223fc7b1d0cb817c01
Jeel Tilva <tilvajeel@gmail.com>
2023-03-02 00:03:07 +0530

    authorizer lambda

--- serviceA/serverless.ts
+++ serviceA/serverless.ts
@@ -50,7 +50,7 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
   },
-  functions: { helloA: hello },
+  functions: { helloA: hello, authorizer: hello },
   resources: {
     Resources: {
       configurationsTable: {
